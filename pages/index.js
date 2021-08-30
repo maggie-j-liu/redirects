ba1ebc062180dd89d3105456279142c4f90fea03
maggie-j-liu <maggie.j.liu@gmail.com>
2021-08-29 18:23:25 -0700

    fix host

--- pages/index.js
+++ pages/index.js
@@ -5,12 +5,13 @@ export default function Home() {
 export function getServerSideProps({ req }) {
   const redirects = {
     'github': 'https://github.com/maggie-j-liu',
+    'gh': 'https://github.com/maggie-j-liu',
     'website': "https://maggieliu.dev"
   }
   console.log(req.headers.host);
   return {
     redirect: {
-      destination: redirects[req.headers.host],
+      destination: redirects[req.headers.host.split('.')[0]],
       permanent: false
     }
   }
